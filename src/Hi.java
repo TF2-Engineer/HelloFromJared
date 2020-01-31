a842ad38b7a66b841487b84ee7d3ba0663a1363b
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-01-31 16:02:53 -0700

    Hello Jared, my name is Elijah

--- src/Hi.java
+++ src/Hi.java
@@ -1,5 +1,7 @@
 public class Hi {
     public static void main(String args[]) {
+
         System.out.println("Hello, my name is Jared");
+        System.out.println("Hello Jared, my name is Elijah");
     }
 }
